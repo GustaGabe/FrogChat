65d5e1a600fe99b7e2e0b2843400964799b118fb
gabrielbueno <gabrielbueno@lugarh.com.br>
2025-04-11 08:38:23 -0300

    feat: add login

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,38 +1,47 @@
 import { useEffect, useState } from 'react'
 import './home.style.scss'
 import { io } from 'socket.io-client'
+import { useNavigate } from 'react-router'
 
 
 const socket = io('http://localhost:3000')
 
 export const Home: React.FC = () => {
   const [ message, setMessage ] = useState<string>('')
-  const [ chat, setChat ] = useState([])
-
+  const [ chat, setChat ] = useState<string[]>([])
+  const [user, setUser] = useState<string>('')
+  const navigate = useNavigate()
 
   useEffect(() => {
     socket.on('chat message', (msg) => {
-      setChat((prev) => [...prev, msg]);
-    });
+      setChat((prev: any) => [...prev, msg]);
+    })
+    const storedUser = window.localStorage.getItem('user');
 
+    if (!storedUser) {
+      navigate('/login');
+    }
+
+    setUser(storedUser || '')
+  
     return () => {
       socket.off('chat message');
     };
-  }, []);
+  }, [user]);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: any) => {
     e.preventDefault();
     socket.emit('chat message', message);
     setMessage('');
   }
 
-  
-  
   return(
     <div className='HomePage__container'>
-      <ul>
+      <ul className='HomePage__messages'>
         {chat.map((msg, index) => (
-          <li key={index} className='HomePage__message'>{msg}</li>
+          <li key={index} className='HomePage__message'>
+            {`${user}: ${msg}`}
+          </li>
         ))}
       </ul>
       <form onSubmit={handleSubmit} className='HomePage__chat-container'>
--- frontend/src/pages/login/login.page.tsx
+++ frontend/src/pages/login/login.page.tsx
@@ -1,18 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './login.style.scss'
+import { useNavigate } from 'react-router'
 
 export const LoginPage: React.FC = () => {
-    const [user, setUser] = useState<boolean>(false)
+    const [hasUser, setHasUser] = useState<boolean>(false)
+    const [user, setUser] = useState<string>('')
+    const navigate = useNavigate()
+    const storedUser = window.localStorage.getItem('user')
+
+    const handleSubmit = (e: any) => {
+      e.preventDefault()
+      setUser('')
+      window.localStorage.setItem('user', user)
+    }
+
+    useEffect(() => {
+      if (storedUser) {
+        navigate('/')
+      }
+    }, [storedUser])
 
     return(
         <div className='LoginPage__container'>
-           {user ? (
+           {hasUser ? (
              <div  className='LoginPage__user'>
                 <span>Digite seu nome de Usuário</span>
-                <input className='LoginPage__input' type="text" />
+                <form onSubmit={handleSubmit}>
+                  <input value={user} onChange={(e) => setUser(e.target.value)} className='LoginPage__input' type="text" />
+                </form>
              </div>
            ) : (
-            <button onClick={() => setUser(true)} className='LoginPage__button'>Login</button>
+            <button onClick={() => setHasUser(true)} className='LoginPage__button'>Login</button>
            )}
         </div>
     )
